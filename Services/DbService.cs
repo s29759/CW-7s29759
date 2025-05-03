5cacafb4f8694ebfede28249f6c9696ca47862e9
s29759 <s29759@pjwstk.edu.pl>
2025-05-03 22:07:21 +0200

    "Commited"

--- Services/DbService.cs
+++ Services/DbService.cs
@@ -16,12 +16,13 @@ public interface IDbService
 public class DbService(IConfiguration configuration) : IDbService
 {
     
-    public async Task<IEnumerable<TripGetDTO>> GetTripsAsync()
+    public async Task<IEnumerable<TripGetDTO>> GetTripsAsync()//pobieranie wszystkich dostępnych wycieczek oraz ich podstawowych informacji
     {
         var result = new List<TripGetDTO>();
         var connectionString = configuration.GetConnectionString("Default");
 
         await using var connection = new SqlConnection(connectionString);
+        //select wszystkich danych z tabeli trip i połączenie z danymi krajów z tabeli Countries
         var sql = " SELECT t.IdTrip, t.Name, t.Description, t.DateFrom, t.DateTo, t.MaxPeople, c.Name AS CountryName FROM Trip t JOIN Country_Trip ct ON ct.IdTrip = t.IdTrip JOIN Country c ON c.IdCountry = ct.IdCountry";
         await using var command = new SqlCommand(sql, connection);
 
@@ -57,13 +58,13 @@ public class DbService(IConfiguration configuration) : IDbService
         return result;
     }
 
-   public async Task<IEnumerable<ClientTripDTO>> GetClientTripsAsync(int idClient)
+   public async Task<IEnumerable<ClientTripDTO>> GetClientTripsAsync(int idClient)//pobieranie wszystkich wycieczek dla danego id klienta
 {
     var result = new List<ClientTripDTO>();
     var connectionString = configuration.GetConnectionString("Default");
 
     await using var connection = new SqlConnection(connectionString);
-
+    //wybranie wszystkich danych z tabeli Trips oraz łączenie z danymi dotyczącymi daty rejestracji oraz płatności z tabeli Client
     var sql = "SELECT t.Name, t.Description, t.DateFrom, t.DateTo, ct.RegisteredAt, ct.PaymentDate FROM Client c JOIN Client_Trip ct ON ct.IdClient = c.IdClient JOIN Trip t ON t.IdTrip = ct.IdTrip WHERE c.IdClient = @IdClient";
 
     await using var command = new SqlCommand(sql, connection);
@@ -112,12 +113,12 @@ public class DbService(IConfiguration configuration) : IDbService
     return result;
 }
 
-public async Task<int> AddClientAsync(ClientPostDTO dto)
+public async Task<int> AddClientAsync(ClientPostDTO dto)//tworzenie nowego klienta
 {
     var connectionString = configuration.GetConnectionString("Default");
     await using var connection = new SqlConnection(connectionString);
-    var sql =
-        "INSERT INTO Client (FirstName, LastName, Email, Telephone, Pesel) OUTPUT INSERTED.IdClient VALUES (@FirstName, @LastName, @Email, @Telephone, @Pesel)";
+    //Insertowanie odpowiednich danych
+    var sql = "INSERT INTO Client (FirstName, LastName, Email, Telephone, Pesel) OUTPUT INSERTED.IdClient VALUES (@FirstName, @LastName, @Email, @Telephone, @Pesel)";
     await using var command = new SqlCommand(sql, connection);
     command.Parameters.AddWithValue("@FirstName", dto.FirstName);
     command.Parameters.AddWithValue("@LastName", dto.LastName);
@@ -130,28 +131,27 @@ public async Task<int> AddClientAsync(ClientPostDTO dto)
     return id;
 }
 
-public async Task<bool> RegisterClientToTripAsync(int clientId, int tripId)
+public async Task<bool> RegisterClientToTripAsync(int clientId, int tripId)//rejestrowanie klienta na konkretną wycieczkę
 {
     var connectionString = configuration.GetConnectionString("Default");
     await using var connection = new SqlConnection(connectionString);
     await connection.OpenAsync();
-
-    var checksql =
-        " SELECT COUNT(1) FROM Client WHERE IdClient = @ClientId; SELECT MaxPeople FROM Trip WHERE IdTrip = @TripId; SELECT COUNT(*) FROM Client_Trip WHERE IdTrip = @TripId;";
+    
+    var checksql = " SELECT COUNT(1) FROM Client WHERE IdClient = @ClientId; SELECT MaxPeople FROM Trip WHERE IdTrip = @TripId; SELECT COUNT(*) FROM Client_Trip WHERE IdTrip = @TripId;";
     await using var command = new SqlCommand(checksql, connection);
     command.Parameters.AddWithValue("@ClientId", clientId);
     command.Parameters.AddWithValue("@TripId", tripId);
     
     using var reader = await command.ExecuteReaderAsync();
-    
+    //sprawdzanie czy znajduje klienta
     if (!reader.Read() || reader.GetInt32(0) == 0)
         throw new NotFoundException("Client not found");
-
+    //sprawdzanie czy znajduje wycieczke
     reader.NextResult();
     if (!reader.Read() || reader.GetInt32(0) == 0)
         throw new NotFoundException("Trip not found");
     int maxPeople = reader.GetInt32(0);
-    
+    //sprawdzanie czy jest miejsce na wycieczce
     reader.NextResult();
     int currentCount = reader.Read() ? reader.GetInt32(0) : 0;
     if(currentCount > maxPeople)
@@ -160,6 +160,7 @@ public async Task<bool> RegisterClientToTripAsync(int clientId, int tripId)
     reader.Close();
     
     int registeredAt = int.Parse(DateTime.Now.ToString("yyyyMMdd"));
+    //wprowadzanie wycieczki dla klienta(rejestrowanie go)
     var insertsql = "INSERT INTO Client_Trip (IdClient, IdTrip, RegisteredAt) VALUES (@ClientId, @TripId, @RegisteredAt)";
     await using var insertCommand = new SqlCommand(insertsql, connection);
     insertCommand.Parameters.AddWithValue("@ClientId", clientId);
@@ -169,10 +170,11 @@ public async Task<bool> RegisterClientToTripAsync(int clientId, int tripId)
     return true;
 }
 
-public async Task<bool> DeleteClientTripAsync(int clientId, int tripId)
+public async Task<bool> DeleteClientTripAsync(int clientId, int tripId)//usuwanie klienta z wycieczki
 {
     var connectionString = configuration.GetConnectionString("Default");
     await using var connection = new SqlConnection(connectionString);
+    //usuwanie klienta z wycieczki
     var sql = "DELETE FROM Client_Trip WHERE IdClient = @ClientId AND IdTrip = @TripId";
     await using var command = new SqlCommand(sql, connection);
     command.Parameters.AddWithValue("@ClientId", clientId);
